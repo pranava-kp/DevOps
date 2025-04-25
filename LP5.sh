978df5942fb1a0a4ca97dba7f3e869ed6dbe0d43
pranava-kp <72963086+pranava-kp@users.noreply.github.com>
2025-04-25 20:40:22 +0530

    Update LP5.sh

--- LP5.sh
+++ LP5.sh
@@ -11,9 +11,14 @@ clear
 echo -e "\e[1;31mSelect Java version 21\e[0m"
 sudo update-alternatives --config java
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]"   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get install jenkins -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
 sudo systemctl start jenkins
 # sudo systemctl status jenkins
 
